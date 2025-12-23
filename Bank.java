a316627cae66def20c7c351d4ca65bb22942a160
LOU Chumdararith <louchumdararith02@gmail.com>
2025-12-24 03:55:03 +0700

    Update after and before lock

--- Bank.java
+++ Bank.java
@@ -1,7 +1,13 @@
 public class Bank implements Runnable {
     static int balance = 0;
 
+    static final Object lock = new Object();
+
     public void deposit() {
+        try {
+            Thread.sleep(10);
+        } catch (Exception e) {
+        }
         balance += 100;
     }
 
@@ -15,10 +21,20 @@ public class Bank implements Runnable {
 
     @Override
     public void run() {
+        // --- CASE 1: BEFORE LOCK (Uncomment this section, Comment out Case 2) ---
+        
+        // deposit();
+        // System.out.println("Value for " + Thread.currentThread().getName() + " after deposit: " + getValue());
+        // withdraw();
+        // System.out.println("Value for " + Thread.currentThread().getName() + " after withdraw: " + getValue());
+         
+
+        // --- CASE 2: AFTER LOCK (Uncomment this section, Comment out Case 1) ---
+        synchronized (lock) {
             deposit();
-            System.out.println("Value for Thread after deposit " + Thread.currentThread().getName() + " " + getValue());
-            
+            System.out.println("Value for " + Thread.currentThread().getName() + " after deposit: " + getValue());
             withdraw();
-            System.out.println("Value for Thread after withdraw " + Thread.currentThread().getName() + " " + getValue());
+            System.out.println("Value for " + Thread.currentThread().getName() + " after withdraw: " + getValue());
+        }
     }
 }
